af7b44a44d385ab72bfe81df54af7d0146f72f06
Bob Rodes <robtrodes@aol.com>
2019-01-10 20:45:21 -0600

    Chapter 2: Variables

---
+++ variables/age.rb
@@ -0,0 +1,6 @@
+print "How old are you? "
+age = gets.chomp.to_i
+print "In 10 years you will be #{age + 10}.\n"
+print "In 20 years you will be #{age + 20}.\n"
+print "In 30 years you will be #{age + 30}.\n"
+print "In 40 years you will be #{age + 40}.\n"
---
+++ variables/name.rb
@@ -0,0 +1,11 @@
+#print "Name? "
+#name = gets.chomp
+#puts "Hello, #{name}!"
+
+#10.times { puts name }
+
+print "First Name? "
+first_name = gets.chomp
+print "Last Name? "
+last_name = gets.chomp
+puts "Ok, so your full name is #{first_name} #{last_name}."
